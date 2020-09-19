a9f9ac1c3c09c05548ee501103094f2f6a49da72
calebeso <calebeso045@gmail.com>
2020-09-19 10:38:31 -0300

    update

---
+++ database/migrations/2020_09_17_235824_create-services-table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function($table)
+        {
+            $table->increments('id');
+            $table->string('description');
+            $table->string('service_note');
+            $table->float('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
