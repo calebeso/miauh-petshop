f766e6a79dec2a8de930a872c96b80caaa775e20
calebeso <calebeso045@gmail.com>
2020-11-13 23:24:55 -0300

    adjusting product seeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -15,28 +15,23 @@ class ProductSeeder extends Seeder
     {
         Product::truncate();
 
-        $category1 = Category::where('id', '1')->first();
-        $category2 = Category::where('id', '2')->first();
-        $category3 = Category::where('id', '3')->first();
-
         $product1 = Product::create([
             'description' => 'Alimento para gato Premium',
             'price' => '77.00',
+            'category_id' => 1
         ]);
 
         $product2 = Product::create([
             'description' => 'Alimento para cachorro',
             'price' => '57.00',
+            'category_id' => 2
         ]);
 
         $product3 = Product::create([
             'description' => 'Casinha para gato',
             'price' => '770.00',
+            'category_id' => 3
         ]); 
-
-        $product1-> categories()->attach($category1);
-        $product2-> categories()->attach($category2);
-        $product3-> categories()->attach($category3);
         
     }
 }
