61bae97cd788dee2c6ebd2c07e72dc94e33e7a9f
calebeso <calebeso045@gmail.com>
2021-01-07 00:00:11 -0300

    validating price field in form modal

--- app/Http/Requests/EventRequest.php
+++ app/Http/Requests/EventRequest.php
@@ -25,13 +25,22 @@ class EventRequest extends FormRequest
     {
         return [
             'title' => 'required|min:3', 
+            'start' => 'date_format:Y-m-d H:i:s|before:end',
+            'end' => 'date_format:Y-m-d H:i:s|after:start',
+            'price' => "required|regex:/^\d+(\.\d{1,2})?$/"
         ];
     }
 
     public function messages(){
         return [ 
-            'title.required' => 'O titulo é obrigatório',
+            'title.required' => 'O titulo é obrigatório!',
             'title.min' => 'Titulo necessita pelo menos 3 caracteres!',
+            'start.date_format' => 'Preencha a data no formato correto!',
+            'start.before' => 'A data inicial deve ser menor que a data final!',
+            'end.date_format' => 'Preencha a data no formato correto!',
+            'end.after' => 'A data final deve ser maior que a data inicial!',
+            'price.required' => 'O campo preço é obrigatório!', 
+            'price.regex' => 'Preencha o campo preço com um valor correto!',
         ];
     }
 }
